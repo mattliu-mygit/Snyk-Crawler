1b126590914d2b201deab4a75a1568ecfb4e21e2
Matthew Liu <mliu78@jh.edu>
2022-04-11 16:18:45 -0400

    Update cleanup.sql

--- processed/cleanup.sql
+++ processed/cleanup.sql
@@ -1,8 +1,8 @@
-DROP TABLE Mental_Hospital;
-DROP TABLE Outpatient;
-DROP TABLE General_Hospital;
-DROP TABLE Day_Treatment;
-DROP TABLE Patient_Ledger;
-DROP TABLE Facility;
-DROP TABLE Suicide_Rates;
-DROP TABLE Country;
+DROP TABLE IF EXISTS Mental_Hospital;
+DROP TABLE IF EXISTS Outpatient;
+DROP TABLE IF EXISTS General_Hospital;
+DROP TABLE IF EXISTS Day_Treatment;
+DROP TABLE IF EXISTS Patient_Ledger;
+DROP TABLE IF EXISTS Facility;
+DROP TABLE IF EXISTS Suicide_Rates;
+DROP TABLE IF EXISTS Country;
