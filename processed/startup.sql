0983fbd690598e5d96c594fc76b38bc2e9ba33c3
Matthew Liu <mliu78@jh.edu>
2022-04-11 00:07:58 -0400

    Update startup.sql

--- processed/startup.sql
+++ processed/startup.sql
@@ -1,3 +1,74 @@
+DROP TABLE Country;
+DROP TABLE Suicide_Rates;
+DROP TABLE Mental_Hospital;
+DROP TABLE Facility;
+DROP TABLE Country;
+DROP TABLE Outpatient;
+DROP TABLE General_Hospital;
+DROP TABLE Day_Treatment;
+CREATE TABLE Country (
+  name varchar(20),
+  region varchar(10),
+  psychiatrist_count float,
+  PRIMARY KEY(name)
+);
+CREATE TABLE Suicide_Rates (
+  country varchar(20),
+  year int,
+  sex varchar(5),
+  age_standardized_suicide_rates float,
+  PRIMARY KEY(country, year, sex),
+  FOREIGN KEY(country) REFERENCES Country(name)
+);
+CREATE TABLE Facility (
+  year int,
+  country varchar(20),
+  type varchar(20),
+  avg_stay int,
+  cost int,
+  unit_count float,
+  PRIMARY KEY(year, country, type),
+  FOREIGN KEY(country) REFERENCES Country(name)
+);
+CREATE TABLE Mental_Hospital (
+  year int,
+  country varchar(20),
+  ptsd_count int,
+  depression_count int,
+  Insanity_count int,
+  PRIMARY KEY(year, country),
+  FOREIGN KEY(country) REFERENCES Facility(country),
+  FOREIGN KEY(year) REFERENCES Facility(year)
+);
+CREATE TABLE Outpatient (
+  year int,
+  country varchar(20),
+  mental_health_allocation float,
+  PRIMARY KEY(year, country),
+  FOREIGN KEY(country) REFERENCES Facility(country),
+  FOREIGN KEY(year) REFERENCES Facility(year)
+);
+CREATE TABLE General_Hospital (
+  year int,
+  country varchar(20),
+  rehabilitation_count int,
+  mental_health_allocation float,
+  mental_health_prescription_count int,
+  PRIMARY KEY(year, country),
+  FOREIGN KEY(country) REFERENCES Facility(country),
+  FOREIGN KEY(year) REFERENCES Facility(year)
+);
+CREATE TABLE Day_Treatment (
+  year int,
+  country varchar(20),
+  closing_hour int,
+  non_drug_alc_count float,
+  frug_alc_count float,
+  PRIMARY KEY(year, country),
+  FOREIGN KEY(country) REFERENCES Facility(country),
+  FOREIGN KEY(year) REFERENCES Facility(year)
+);
+LOAD DATA LOCAL INFILE './country.csv' INTO TABLE Country FIELDS TERMINATED BY ',';
 LOAD DATA LOCAL INFILE './suicide_rates.csv' INTO TABLE Suicide_Rates FIELDS TERMINATED BY ',';
 LOAD DATA LOCAL INFILE './facility.csv' INTO TABLE Facility FIELDS TERMINATED BY ',';
 LOAD DATA LOCAL INFILE './mh.csv' INTO TABLE Mental_Hospital FIELDS TERMINATED BY ',';
