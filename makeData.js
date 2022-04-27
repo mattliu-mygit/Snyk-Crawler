9a60e1b1f8a25c96d94cbe34348df440e5a6cce8
Matthew Liu <104270646+mliu78@users.noreply.github.com>
2022-04-27 12:56:41 -0400

    updated data to have rehabilitation amount to be less than diagnoses count

--- makeData.js
+++ makeData.js
@@ -230,7 +230,7 @@ toJSON()
                                   ),
                                 });
                               mhuJSON.rehabilitation_count = Math.floor(
-                                Math.random() * 100
+                                Math.random() * 50 + 50
                               );
                               mhuJSON.mental_health_allocation = (
                                 Math.random() * 0.5
@@ -242,9 +242,7 @@ toJSON()
                                 year: mhuJSON.Period,
                                 country: mhuJSON.Location,
                                 cost: Math.floor(Math.random() * 50),
-                                diagnoses_count: Math.floor(
-                                  Math.random() * 100
-                                ),
+                                diagnoses_count: Math.floor(Math.random() * 50),
                                 patient_count: Math.floor(Math.random() * 75),
                               });
                             });
