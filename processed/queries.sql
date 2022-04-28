7c9cf5973bbe29ef1acd9d64a16930f9b401a2f6
Matthew Liu <104270646+mliu78@users.noreply.github.com>
2022-04-28 13:34:48 -0400

    another query

--- processed/queries.sql
+++ processed/queries.sql
@@ -119,3 +119,28 @@ SELECT region,
 FROM Country
   JOIN Suicide_Rates ON Country.name = Suicide_Rates.country
 GROUP BY region;
+-- 8. Spending the most on what type of facility is correlated the lowest amounts of suicide_rates?
+WITH FacilityCount as (
+  SELECT name,
+    MAX(Facility.cost) as max_cost,
+    facility_type
+  FROM Country
+    JOIN Facility ON Country.name = Facility.country
+  GROUP BY name
+),
+AverageSuicide as (
+  SELECT country,
+    AVG(age_standardized_suicide_rates) as suicide_rates_avg
+  FROM Suicide_Rates
+  GROUP BY country
+),
+MinRates as (
+  SELECT facility_type,
+    MIN(suicide_rates_avg) as min_rate
+  FROM AverageSuicide
+    JOIN FacilityCount ON name = country
+  GROUP BY facility_type
+)
+SELECT facility_type,
+  MIN(min_rate)
+FROM MinRates;
