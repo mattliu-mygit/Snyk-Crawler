d717164ebf0430b2c9efaa6322e9699c931a46f4
Matthew Liu <104270646+mliu78@users.noreply.github.com>
2022-04-27 22:09:02 -0400

    update.js

--- dbase-mliu78.session.sql
+++ dbase-mliu78.session.sql
@@ -1,6 +1,2 @@
--- 7. What region has the highest rates of depression?
-SELECT region,
-  AVG(age_standardized_suicide_rates) AS available_mental_health_units
-FROM Country
-  JOIN Suicide_Rates ON Country.name = Suicide_Rates.country
-GROUP BY region;
+DELETE FROM Country
+WHERE name = 'New Country Name';
---
+++ processed/updates.sql
@@ -0,0 +1,197 @@
+-- Karen He, khe8
+-- Matthew Liu, mliu78
+/* Insertion */
+-- Country Insertion
+INSERT INTO Country
+VALUES (
+    'New Country Name',
+    'Western Pacific',
+    2.27,
+    357109
+  );
+-- Suicide_Rates Insertion
+INSERT INTO Suicide_Rates
+VALUES (
+    2019,
+    'New Country Name',
+    'male',
+    12.56
+  );
+/* To insert a facility for each type of facility */
+-- Mental_Hospital Facility Insertion
+INSERT INTO Facility
+VALUES (
+    'mental hospital',
+    2019,
+    'New Country Name',
+    110,
+    24,
+    0.47
+  );
+-- Outpatient Facility Insertion
+INSERT INTO Facility
+VALUES (
+    'outpatient facility',
+    2019,
+    'New Country Name',
+    0,
+    0,
+    0
+  );
+-- General_Hospital Facility Insertion
+INSERT INTO Facility
+VALUES (
+    'mental health unit',
+    2019,
+    'New Country Name',
+    0,
+    0,
+    0
+  );
+-- Day_Treatment Facility Insertion
+INSERT INTO Facility
+VALUES (
+    'day treatment facility',
+    2019,
+    'New Country Name',
+    0,
+    0,
+    0
+  );
+-- Mental_Hospital Insertion
+INSERT INTO Mental_Hospital
+VALUES (
+    2019,
+    'New Country Name',
+    0,
+    0,
+    0
+  );
+-- Outpatient Insertion
+INSERT INTO Outpatient
+VALUES (
+    2019,
+    'New Country Name',
+    0.0
+  );
+-- General_Hospital Insertion
+INSERT INTO General_Hospital
+VALUES (
+    2019,
+    'New Country Name',
+    0,
+    0.0,
+    0
+  );
+-- Day_Treatment Insertion
+INSERT INTO Day_Treatment
+VALUES (
+    2019,
+    'New Country Name',
+    0,
+    0.0
+  );
+/* To insert a patient ledger for each type of facility */
+-- Mental_Hospital Patient_Ledger Insertion
+INSERT INTO Patient_Ledger
+VALUES (
+    'mental hospital',
+    2019,
+    'New Country Name',
+    0,
+    0,
+    0
+  );
+-- Outpatient Patient_Ledger Insertion
+INSERT INTO Patient_Ledger
+VALUES (
+    'outpatient facility',
+    2019,
+    'New Country Name',
+    0,
+    0,
+    0
+  );
+-- General_Hospital Patient_Ledger Insertion
+INSERT INTO Patient_Ledger
+VALUES (
+    'mental health unit',
+    2019,
+    'New Country Name',
+    0,
+    0,
+    0
+  );
+-- Day_Treatment Patient_Ledger Insertion
+INSERT INTO Patient_Ledger
+VALUES (
+    'day treatment facility',
+    2019,
+    'New Country Name',
+    0,
+    0,
+    0
+  );
+/* Deletion */
+-- Mental_Hospital Patient_Ledger Deletion
+DELETE FROM Patient_Ledger
+WHERE year = 2019
+  AND country = 'New Country Name'
+  AND facility_type = 'mental hospital';
+-- Outpatient Patient_Ledger Deletion
+DELETE FROM Patient_Ledger
+WHERE year = 2019
+  AND country = 'New Country Name'
+  AND facility_type = 'outpatient facility';
+-- General_Hospital Patient_Ledger Deletion
+DELETE FROM Patient_Ledger
+WHERE year = 2019
+  AND country = 'New Country Name'
+  AND facility_type = 'mental health unit';
+-- Day_Treatment Patient_Ledger Deletion
+DELETE FROM Patient_Ledger
+WHERE year = 2019
+  AND country = 'New Country Name'
+  AND facility_type = 'day treatment facility';
+-- Mental_Hospital Deletion
+DELETE FROM Mental_Hospital
+WHERE year = 2019
+  AND country = 'New Country Name';
+-- Outpatient Deletion
+DELETE FROM Outpatient
+WHERE year = 2019
+  AND country = 'New Country Name';
+-- General_Hospital Deletion
+DELETE FROM General_Hospital
+WHERE year = 2019
+  AND country = 'New Country Name';
+-- Day_Treatment Deletion
+DELETE FROM Day_Treatment
+WHERE year = 2019
+  AND country = 'New Country Name';
+-- Mental_Hospital Facility Deletion
+DELETE FROM Facility
+WHERE year = 2019
+  AND country = 'New Country Name'
+  AND facility_type = 'mental hospital';
+-- Outpatient Facility Deletion
+DELETE FROM Facility
+WHERE year = 2019
+  AND country = 'New Country Name'
+  AND facility_type = 'outpatient facility';
+-- General_Hospital Facility Deletion
+DELETE FROM Facility
+WHERE year = 2019
+  AND country = 'New Country Name'
+  AND facility_type = 'mental health unit';
+-- Day_Treatment Facility Deletion
+DELETE FROM Facility
+WHERE year = 2019
+  AND country = 'New Country Name'
+  AND facility_type = 'day treatment facility';
+-- Suicide_Rates Deletion
+DELETE FROM Country
+WHERE name = 'New Country Name';
+-- Country Deletion
+DELETE FROM Country
+WHERE name = 'New Country Name';
