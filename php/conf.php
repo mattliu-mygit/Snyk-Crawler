bfad3c768b4ba6983072eff71e9eaae4b491db6d
Karen <khe8@jhu.edu>
2022-05-08 13:29:08 -0400

    conf.php

--- php/conf.php
+++ php/conf.php
@@ -1,6 +1,6 @@
 <?php
   $dbhost = "dbase.cs.jhu.edu";
-  $dbuser = "22sp_mliu78";
-  $dbpass = "sJFZiyQgro";
-  $dbname = "22sp_mliu78_db";
+  $dbuser = "22sp_khe8";
+  $dbpass = "0iXBpeXEQR";
+  $dbname = "22sp_khe8_db";
 ?>
