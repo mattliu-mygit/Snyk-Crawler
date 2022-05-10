fc0e720ee149d274ba19b2f3db5ca592a6aee3a7
Matthew Liu <mliu78@jh.edu>
2022-05-10 13:15:02 -0400

    bruh

--- php/Startup.php
+++ php/Startup.php
@@ -6,7 +6,12 @@
     include "open.php";
     try {
       $sql = file_get_contents('../processed/startup.sql');
-      $passCheck = $conn->multi_query($sql);
+      // echo $sql;
+      $result = $conn->multi_query($sql); 
+      echo $result;
+      // $conn->close();
+      // include "open.php";     
+      // $conn->multi_query("LOAD DATA LOCAL INFILE './country.csv' INTO TABLE Country FIELDS TERMINATED BY ',';LOAD DATA LOCAL INFILE './suicide.csv' INTO TABLE Suicide_Rates FIELDS TERMINATED BY ',';LOAD DATA LOCAL INFILE './facility.csv' INTO TABLE Facility FIELDS TERMINATED BY ',';");
       echo "Startup complete";
     } catch(Exception $e) {
       echo "ERROR: startup invalid ".$e;
