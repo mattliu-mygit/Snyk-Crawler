f9948c75cb25433195ca348d451b675fbee533f0
Matthew Liu <mliu78@jh.edu>
2022-05-10 14:18:56 -0400

    movingin everythign to 1 folder

--- php/Startup.php
+++ php/Startup.php
@@ -5,13 +5,10 @@
   <?php
     include "open.php";
     try {
-      $sql = file_get_contents('../processed/startup.sql');
+      $sql = file_get_contents('/startup.sql');
       // echo $sql;
-      $result = $conn->multi_query($sql); 
-      echo $result;
-      // $conn->close();
-      // include "open.php";     
-      // $conn->multi_query("LOAD DATA LOCAL INFILE './country.csv' INTO TABLE Country FIELDS TERMINATED BY ',';LOAD DATA LOCAL INFILE './suicide.csv' INTO TABLE Suicide_Rates FIELDS TERMINATED BY ',';LOAD DATA LOCAL INFILE './facility.csv' INTO TABLE Facility FIELDS TERMINATED BY ',';");
+      // echo $sql;
+      $conn->multi_query($sql);
       echo "Startup complete";
     } catch(Exception $e) {
       echo "ERROR: startup invalid ".$e;
